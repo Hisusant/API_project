ed35699992b8e3d91c8f93c9db3462269ec06f6b
hisusant <spsusantkumar@gmail.com>
2022-08-20 18:13:27 +0530

    api_project

--- test1.py
+++ test1.py
@@ -39,5 +39,24 @@ def root1():
         b = request.json['num2']
         result = a**b
         return jsonify((str(result)))
+
+
+@app.route('/dbconn1', methods=['GET','POST'])
+def mysql_dbconn():
+    if (request.method=='POST'):
+        try:
+            engine = conn.connect(host='localhost', user='root', passwd='susant123')
+            #establish connection
+            cursor = engine.cursor()  # creating cursor object
+            query1 = request.json['query_db'] # "query_db" = "create database API_DB"
+            query2 = request.json['query_table'] # "query_table" = "create table api_table"
+            cursor.execute(query1)
+            cursor.execute(query2)
+            result = "database and table connected"
+            engine.close()
+            return jsonify((str(result)))
+        except Exception as e:
+            return jsonify((str(e)))
+
 if __name__ == '__main__':
     app.run()
---
+++ test2.py
@@ -0,0 +1,31 @@
+import mysql.connector as conn
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route('/abc1/root12', methods=['GET','POST'])
+def root1():
+    if (request.method=='POST'):
+        a = request.json['num1']
+        b = request.json['num2']
+        result = a**b
+        return jsonify((str(result)))
+
+@app.route('/dbconn1', methods=['GET','POST'])
+def mysql_dbconn():
+    if (request.method=='POST'):
+        try:
+            mydb = conn.connect(host='localhost', user='root', passwd='susant123')
+            #establish connection
+            cursor = mydb.cursor()  # creating cursor object
+            query1 = request.json['query_db'] # "query_db" : "create database API_DB"
+            query2 = request.json['query_table'] # "query_table" : "create table api_table"
+            cursor.execute(query1)
+            cursor.execute(query2)
+            result = "database and table connected"
+            mydb.close()
+            return jsonify((str(result)))
+        except Exception as e:
+            return jsonify((str(e)))
+
+if __name__ == '__main__':
+    app.run()
