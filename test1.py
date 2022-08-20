42e5e4355ad4e1ddffc68b6bd9414d7250b5cd05
hisusant <spsusantkumar@gmail.com>
2022-08-20 17:08:01 +0530

    api_project

---
+++ test1.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+@app.route('/abc', methods=['GET','POST'])
+def test():
+    if (request.method=='POST'):
+        a = request.json['num1']
+        b = request.json['num2']
+        result = a+b
+        return jsonify((str(result)))
+
+@app.route('/abc1/multiplication', methods=['GET','POST'])
+def multiplication1():
+    if (request.method=='POST'):
+        a = request.json['num1']
+        b = request.json['num2']
+        result = a*b
+        return jsonify((str(result)))
+
+
+
+
+if __name__ == '__main__':
+    app.run()
